479cbd1c569631e51fc15875ede99fb0362a6337
Zawar Mehmood <43095019+zeddyyz@users.noreply.github.com>
2021-05-07 04:43:06 -0400

    v1.2

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ document.getElementById("results_card").style.display = "none";
 
 function calculate_amount() {
     document.getElementById("results_card").style.display = "block";
+    var doc = document.getElementById("results_card");
+    doc.classList.add('animate__animated', 'animate__fadeInDown');
 
     var account_size = parseFloat(document.getElementById("account_size").value);
     var risk_percent = parseFloat(document.getElementById("risk_percent").value);
@@ -15,9 +17,7 @@ function calculate_amount() {
     var loss_amount = (entry_price - stop_loss) * shares_amount * currency_conversion;
     var profit_amount = (target_price - entry_price) * shares_amount * currency_conversion;
 
-
     document.getElementById("risk_amount_output").innerHTML = "$" + risk_amount.toFixed(2);
     document.getElementById("shares_amount_output").innerHTML = Math.round(shares_amount);
     document.getElementById("profit_amount_output").innerHTML = "$" + profit_amount.toFixed(2);
-
 }
